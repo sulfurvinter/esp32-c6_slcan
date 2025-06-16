50860be2a08babac5fd380f0b7bdbf14f2a0428a
sulfurvinter <linda.ljungdahl@gmail.com>
2025-06-16 20:11:53 +0200

    full support added

--- esp32-c6_slcan.ino
+++ esp32-c6_slcan.ino
@@ -1,10 +1,8 @@
 #include <ESP32-TWAI-CAN.hpp>
 #include <Adafruit_NeoPixel.h>
 
-// Simple sketch that querries OBD2 over CAN for coolant temperature
-// Showcasing simple use of ESP32-TWAI-CAN library driver.
+// Showcasing simple use of ESP32-TWAI-CAN library to emulate lawicel slcan tranciever
 
-// Default for ESP32
 #define CAN_TX		19
 #define CAN_RX		20
 
@@ -54,9 +52,6 @@ typedef struct
 //    uint8_t data[TWAI_FRAME_MAX_DLC];    /**< Data bytes (not relevant in RTR frame) */
 //} twai_message_t;
 
-
-
-//----------------------------------- b2ahex(p, 2, msg.header.length, msg.data);
 
 // conversion from hex to asci-hex, p=output buffer, s=type of conversion (1=1 byte,2=),  n=number of bytes to convert
 int b2ahex(char *p, uint8_t s, uint8_t n, void *v)
@@ -138,7 +133,6 @@ int a2bhex(char *p, uint8_t s, uint8_t n, void *v)
 // transfer messages from CAN bus to host
 void xfer_can2tty()
 {
-  //tCAN msg;
   CanFrame rxframe2;
   char buf[CMD_LEN];
   int i;
@@ -217,7 +211,6 @@ void slcan_nack()
 
 void send_canmsg(char *buf)
 {
-  //tCAN msg;
 	CanFrame TxFrame = { 0 };
   int len = strlen(buf) - 1;
   uint16_t id[2];
@@ -257,23 +250,6 @@ void send_canmsg(char *buf)
       Serial.print("Z");
     }
   }
-
-
-/*
-	TxFrame.identifier = 0x7DF; // Default OBD2 address;
-	TxFrame.extd = 0;
-	TxFrame.data_length_code = 8;
-	TxFrame.data[0] = 2;
-	TxFrame.data[1] = 1;
-	TxFrame.data[2] = obdId;
-	TxFrame.data[3] = 0xAA;    // Best to use 0xAA (0b10101010) instead of 0
-	TxFrame.data[4] = 0xAA;    // CAN works better this way as it needs
-	TxFrame.data[5] = 0xAA;    // to avoid bit-stuffing
-	TxFrame.data[6] = 0xAA;
-	TxFrame.data[7] = 0xAA;
-    // Accepts both pointers and references 
-    ESP32Can.writeFrame(TxFrame,0);  // timeout defaults to 1 ms
-*/
 }
 
 void pars_slcancmd(char *buf)
@@ -411,31 +387,8 @@ void xfer_tty2can()
   }
 }
 
-
-//--------------------------------
-
-
-void sendObdFrame(uint8_t obdId)
-{
-	CanFrame obdFrame = { 0 };
-	obdFrame.identifier = 0x7DF; // Default OBD2 address;
-	obdFrame.extd = 0;
-	obdFrame.data_length_code = 8;
-	obdFrame.data[0] = 2;
-	obdFrame.data[1] = 1;
-	obdFrame.data[2] = obdId;
-	obdFrame.data[3] = 0xAA;    // Best to use 0xAA (0b10101010) instead of 0
-	obdFrame.data[4] = 0xAA;    // CAN works better this way as it needs
-	obdFrame.data[5] = 0xAA;    // to avoid bit-stuffing
-	obdFrame.data[6] = 0xAA;
-	obdFrame.data[7] = 0xAA;
-    // Accepts both pointers and references 
-    ESP32Can.writeFrame(obdFrame,0);  // timeout defaults to 1 ms
-}
-
 void setup()
 {
-
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   
   pixels.setPixelColor(0, pixels.Color(150, 0, 0));
@@ -452,6 +405,7 @@ void setup()
 
   // Setup serial for debbuging.
   Serial.begin(1000000);
+  Serial1.begin(9600);
 
   // Set pins
 	ESP32Can.setPins(CAN_TX, CAN_RX);
@@ -479,36 +433,10 @@ void setup()
   // or override everything in one command;
   // It is also safe to use .begin() without .end() as it calls it internally
   //if(ESP32Can.begin(ESP32Can.convertSpeed(500), CAN_TX, CAN_RX, 10, 10))
-  //{
-  //    Serial.println("CAN bus started!");
-  //}
-  //else
-  //{
-  //  Serial.println("CAN bus failed!");
-  // }
 }
 
 void loop()
 {
-    static uint32_t lastStamp = 0;
-    uint32_t currentStamp = millis();
-
   xfer_can2tty();
   xfer_tty2can();
-
-
-
-    //if(currentStamp - lastStamp > 1000) {   // sends OBD2 request every second
-    //    lastStamp = currentStamp;
-    //    sendObdFrame(5); // For coolant temperature
-    //}
-
-    // You can set custom timeout, default is 1000
-   // if(ESP32Can.readFrame(rxFrame, 1000)) {
-   //     // Comment out if too many frames
-   //     Serial.printf("Received frame: %03X  \r\n", rxFrame.identifier);
-   //     if(rxFrame.identifier == 0x7E8) {   // Standard OBD2 frame responce ID
-   //         Serial.printf("Collant temp: %3d°C \r\n", rxFrame.data[3] - 40); // Convert to °C
-   //     }
-   // }
 }
